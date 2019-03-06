86017db76a77b5c57c905fc96e65f597640f6b34
Andrew Benedict <atbenedict@gmail.com>
2019-03-06 17:36:40 -0500

    Completed Display Tests

--- src/controls/Controls.spec.js
+++
@@ -1,6 +0,0 @@
-// Test away!
-
-// - provide buttons to toggle the`closed` and`locked` states.
-// - buttons' text changes to reflect the state the door will be in if clicked
-//   - the closed toggle button is disabled if the gate is closed
-//     - the locked toggle button is disabled if the gate is open
